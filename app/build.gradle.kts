a608c54cb81df45b8b7371337276e27ad575a5eb
Murari <murarikumar42@gmail.com>
2025-12-31 05:29:17 +0530

    FIX: Update Version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.murari.careerpolitics"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 6
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
