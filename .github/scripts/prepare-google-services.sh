e699d6cfc2b7295227f4d35c9c04050658a7941b
Murari Kumar <muraridevv@gmail.com>
2026-02-24 13:48:50 +0530

    Fix CI placeholder google-services for debug and staging

--- .github/scripts/prepare-google-services.sh
+++ .github/scripts/prepare-google-services.sh
@@ -20,9 +20,47 @@ cat > "$TARGET_FILE" <<'JSON'
   "client": [
     {
       "client_info": {
-        "mobilesdk_app_id": "1:000000000000:android:placeholder",
+        "mobilesdk_app_id": "1:000000000000:android:placeholderrelease",
         "android_client_info": {
           "package_name": "com.murari.careerpolitics"
+        }
+      },
+      "oauth_client": [],
+      "api_key": [
+        {
+          "current_key": "placeholder"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": []
+        }
+      }
+    },
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:000000000000:android:placeholderdebug",
+        "android_client_info": {
+          "package_name": "com.murari.careerpolitics.debug"
+        }
+      },
+      "oauth_client": [],
+      "api_key": [
+        {
+          "current_key": "placeholder"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": []
+        }
+      }
+    },
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:000000000000:android:placeholderstaging",
+        "android_client_info": {
+          "package_name": "com.murari.careerpolitics.staging"
         }
       },
       "oauth_client": [],
@@ -42,4 +80,4 @@ cat > "$TARGET_FILE" <<'JSON'
 }
 JSON
 
-echo "GOOGLE_SERVICES_JSON_BASE64 not set; using CI placeholder google-services.json."
+echo "GOOGLE_SERVICES_JSON_BASE64 not set; using CI placeholder google-services.json for release/debug/staging packages."
